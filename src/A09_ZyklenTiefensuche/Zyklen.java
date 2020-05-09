231206397cc090d5a18cb2e1a5df9929c88b6613
peter_deutsch <peter.deutsch@avl.com>
2020-05-09 13:28:52 +0200

    Initial commit

---
+++ src/A09_ZyklenTiefensuche/Zyklen.java
@@ -0,0 +1,26 @@
+package A09_ZyklenTiefensuche;
+
+import java.util.List;
+import basisAlgorithmen.*;
+
+public class Zyklen {
+
+	private Graph g;
+	
+	public Zyklen(Graph g) {
+		this.g = g;
+	}
+	
+	/**
+	 * Retourniert einen Zyklus eines Graphen, sofern einer existiert
+	 * @param g zu prüfender Graph
+	 * @return Anzahl der Komponenten
+	 */
+	public List<Integer> getCycle() {
+
+		return null;
+	}
+	
+	
+
+}
