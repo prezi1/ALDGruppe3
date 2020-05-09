eeb71bdf9ad1627640ec5a854523a2ca08486525
Lukas Hödl <lukas.hoedl@outlook.com>
2020-05-09 14:45:19 +0200

    Implementierung Stack Beispiel 1

--- src/A01_Stack/Stack.java
+++ src/A01_Stack/Stack.java
@@ -4,6 +4,7 @@ package A01_Stack;
 public class Stack<T>
 {
 	 private Node<T> first;
+	 private int counter;
     /**
      * Oberstes Element entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
@@ -11,7 +12,16 @@ public class Stack<T>
      */
     public T pop() throws StackEmptyException {
 
-    	return null;
+        // Falls keine First Node vorhanden ist == Stack ist Leer
+        if(first == null){
+            throw new StackEmptyException();
+        }
+       // Oberste Element zwischen speichern
+        Node<T> result = first;
+        // Darunterliegende  Element auf First setzen
+        first = first.getNext();
+        counter--;
+        return  result.getData();
     }
     
     /**
@@ -19,6 +29,21 @@ public class Stack<T>
      * @param i data
      */
     public void push(T i) {
+        counter++;
+        // Neues Node Objekt erzeugen
+
+        Node<T> n = new Node(i);
+        // Falls Stack leer dann ist Node == Erstes Element im Stack
+        if(first ==  null){
+            first = n;
+        }
+        // Wenn Element vorhanden dann, aktuelles Element wird der Zeiger auf darunterliegendesw Element gesetzt.
+        else {
+            n.setNext(first);
+           // Aktuelles Element wird zum obersten Element !!! Happy Coding !!!
+            first = n;
+        }
+
 
     }
     
@@ -27,6 +52,6 @@ public class Stack<T>
      * @return
      */
     public int getCount() {
-    	return 0;
+    	return counter;
     }
 }
