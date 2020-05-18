17bd78a2b2d9dcb510dcd0e06503f822f6555454
peter_deutsch <peter.deutsch@avl.com>
2020-05-18 11:40:42 +0200

    HandOut Stack

---
+++ src/A02_Heap/TaskHeapArrayList.java
@@ -0,0 +1,36 @@
+package A02_Heap;
+
+import java.util.ArrayList;
+
+public class TaskHeapArrayList {
+
+	/**
+	 * Internes Task-Array für den Heap
+	 * Ansonsten keine anderen Variablen verwenden!
+	 */
+	private ArrayList<Task> tasks;
+
+	/**
+	 * Konstruktor
+	 */
+	public TaskHeapArrayList() {
+
+	}
+
+	/**
+	 * Neuen Task in den Heap einfügen
+	 * @param t Einzufügender Task
+	 */
+	public void insert(Task t) {
+
+	}
+
+	/**
+	 * Das oberste Element (mit kleinster Priorität entfernen)
+	 * @return Task mit kleinster Priorität
+	 */
+	public Task remove() {
+		return null;
+	}
+
+}
