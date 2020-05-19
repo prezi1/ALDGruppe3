176e11e62012ffb3db49e046200a8aea6bed96dd
Lukas Hödl <lukas.hoedl@outlook.com>
2020-05-19 19:49:08 +0200

    Implementierung Queue Beispiel 2

--- src/A02_Queue/Queue.java
+++ src/A02_Queue/Queue.java
@@ -1,62 +1,63 @@
 package A02_Queue;
 
-public class Queue<T>
-{
+public class Queue<T> {
     private Node<T> first;
     private int counter;
-    
+
     private Node<T> last;
 
     /**
      * Das vorderste (=erste) Element aus der Queue entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
-     * @throws QueueEmptyException 
+     *
+     * @throws QueueEmptyException
      */
     public T dequeue() throws QueueEmptyException {
         // Exception wenn das erste Element null ist
 
 
-        if(first == null){
+        if (first == null) {
             throw new QueueEmptyException();
         }
 
+        // First Node für Return auf temp Node legen
         Node<T> temp;
         temp = first;
+        // Neue first Node auf Vorgänger setzen
         first = first.getNext();
         counter--;
 
-    	return (T) temp.getData();
+        return (T) temp.getData();
     }
-    
-    
-    
+
+
     /**
      * Übergebenen Integer am Ende der Queue anhängen.
+     *
      * @param i Zahl
      */
     public void enqueue(T i) {
 
         Node<T> n = new Node(i);
         counter++;
-        if(first == null){
+
+        // Wenn first Null ist dan ist First und Last == n
+        if (first == null) {
             first = n;
-            last = n;
 
-        }else{
-
-                last.setNext(n);
-                last = n;
-
+        } else {
+            last.setNext(n); // Vorletztes Element zeigt auf das letzte Element ! ! Happy Coding ! !
         }
-
+        last = n;
 
     }
-    
+
     /**
      * Liefert die Anzahl der Elemente im Stack
+     *
      * @return
      */
     public int getCount() {
-    	return this.counter;
+        return this.counter;
     }
 }
